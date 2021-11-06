bc88e8a3b77b071e74ea3626c3b5994b0ef17bba
mjjstockman <mjjstockman@gmail.com>
2021-11-06 12:07:24 +0000

    Wire up APIs and confirm access to google sheet

--- run.py
+++ run.py
@@ -1,3 +1,24 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+# needed for google sheets api
+import gspread
+from google.oauth2.service_account import Credentials
+
+# IAM config allowing user access 
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file("creds.json")
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open("game_rental")
+
+games = SHEET.worksheet("games")
+
+data = games.get_all_values()
+print(data)
