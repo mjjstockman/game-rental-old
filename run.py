1c912383109504cb6868707623a23158cc849c08
mjjstockman <mjjstockman@gmail.com>
2021-11-06 12:45:28 +0000

    Create choose_action function to get action choice from user

--- run.py
+++ run.py
@@ -20,5 +20,21 @@ SHEET = GSPREAD_CLIENT.open("game_rental")
 
 games = SHEET.worksheet("games")
 
-data = games.get_all_values()
-print(data)
+# data = games.get_all_values()
+# print(data)
+
+def choose_action():
+    """
+    Display initial options to user.
+    Get desiered action input from user via the terminal, 
+    which must be an integer between 1 and 5.
+    The loop will repeatedly request data, until it is valid.
+    """
+    print("Do you want to:\n 1) Make a sale.\n 2) Return a sale.\n 3) Check stock?\n 4) Add a new customer.\n 5) Add a new title.\n")
+    # print("Please select from above by entering the corresponding number.")
+
+    chosen_action = input("Please select from above by entering the corresponding number: ")
+    print(chosen_action)
+
+
+choose_action()
